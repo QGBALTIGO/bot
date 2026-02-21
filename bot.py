c5f8358af998ddb85a3e475a6ecf24a902f5d464
QGBALTIGO <119024696+QGBALTIGO@users.noreply.github.com>
2026-02-21 15:46:55 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,102 +1,3 @@
-import os
-from pyrogram import Client
-from aiohttp import ClientSession
-
-TRIGGERS = os.environ.get("TRIGGERS", "/ !").split()
-API_HASH = os.environ.get("22be457ce73f65fad82315073fbc3")
-BOT_TOKEN = os.environ.get("8001392073:AAEW64SRZI7BIY6l8reeKnNONu-6gjLt0Sg")
-BOT_NAME = os.environ.get("@SourceBaltigo_Bot")
-DB_URL = os.environ.get("DATABASE_URL=mongodb+srv://kaykys468_db_user:oqSNFQ0Su4uWCcRT@cluster0.xxxxx.mongodb.net/anibot")
-ANILIST_CLIENT = os.environ.get("36318")
-ANILIST_SECRET = os.environ.get("0S7GDyIMBRrvmQI8M2Mc32FIdC1j9wgK4WVhUe0M")
-ANILIST_REDIRECT_URL = os.environ.get("Ahttps://example.com", "https://anilist.co/api/v2/oauth/pin")
-API_ID = int(os.environ.get("34116600"))
-LOG_CHANNEL_ID = int(os.environ.get("-1003889677462"))
-OWNER = list(filter(lambda x: x, map(int, os.environ.get("1852596083", "1005170481 804248372 1993696756").split())))  ## sudos can be included
-
-DOWN_PATH = "anibot/downloads/"
-HELP_DICT = dict()
-
-session = ClientSession()
-plugins = dict(root="anibot/plugins")
-anibot = Client("anibot", bot_token=8001392073:AAEW64SRZI7BIY6l8reeKnNONu-6gjLt0Sg, api_id=34116600, api_hash=22be457ce73f65fad82315073fbc3, plugins=plugins)
-
-has_user: bool = False
-if os.environ.get('silasemanueI'):
-    has_user: bool = True
-    user = Client(os.environ.get('silasemanueI'), api_id=34116600, api_hash=22be457ce73f65fad82315073fbc3)
-
-HELP_DICT['Group'] = '''
-Group based commands:
-
-/settings - Toggle stuff like whether to allow 18+ stuff in group or whether to notify about aired animes, etc and change UI
-
-/disable - Disable use of a cmd in the group (Disable multiple cmds by adding space between them)
-`/disable anime anilist me user`
-
-/enable - Enable use of a cmd in the group (Enable multiple cmds by adding space between them)
-`/enable anime anilist me user`
-
-/disabled - List out disabled cmds
-'''
-
-HELP_DICT["Additional"] = """Use /reverse cmd to get reverse search via tracemoepy API
-__Note: This works best on uncropped anime pic,
-when used on cropped media, you may get result but it might not be too reliable__
-
-Use /schedule cmd to get scheduled animes based on weekdays
-
-Use /watch cmd to get watch order of searched anime
-
-Use /fillers cmd to get a list of fillers for an anime
-
-Use /quote cmd to get a random quote
-"""
-
-HELP_DICT["Anilist"] = """
-Below is the list of basic anilist cmds for info on anime, character, manga, etc.
-
-/anime - Use this cmd to get info on specific anime using keywords (anime name) or Anilist ID
-(Can lookup info on sequels and prequels)
-
-/anilist - Use this cmd to choose between multiple animes with similar names related to searched query
-(Doesn't includes buttons for prequel and sequel)
-
-/character - Use this cmd to get info on character
-
-/manga - Use this cmd to get info on manga
-
-/airing - Use this cmd to get info on airing status of anime
-
-/top - Use this cmd to lookup top animes of a genre/tag or from all animes
-(To get a list of available tags or genres send /gettags or /getgenres
-'/gettags nsfw' for nsfw tags)
-
-/user - Use this cmd to get info on an anilist user
-
-/browse - Use this cmd to get updates about latest animes
-"""
-
-HELP_DICT["Oauth"] = """
-This includes advanced anilist features
-
-Use /auth or !auth cmd to get details on how to authorize your Anilist account with bot
-Authorising yourself unlocks advanced features of bot like:
-- adding anime/character/manga to favourites
-- viewing your anilist data related to anime/manga in your searches which includes score, status, and favourites
-- unlock /flex, /me, /activity and /favourites commands
-- adding/updating anilist entry like completed or plan to watch/read
-- deleting anilist entry
-
-Use /flex or !flex cmd to get your anilist stats
-
-Use /logout or !logout cmd to disconnect your Anilist account
-
-Use /me or !me cmd to get your anilist recent activity
-Can also use /activity or !activity
-
-Use /favourites or !favourites cmd to get your anilist favourites
-
 from telethon import TelegramClient
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
@@ -198,6 +99,7 @@ app.add_handler(CommandHandler("manga", manga))
 
 print("🤖 Bot rodando...")
 app.run_polling()
+
 
 
 
