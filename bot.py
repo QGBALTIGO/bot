976f8ef00f0d03727e92b4655af11510f224f0fc
QGBALTIGO <119024696+QGBALTIGO@users.noreply.github.com>
2026-02-22 19:51:47 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -264,9 +264,6 @@ async def buscar_anilist_manga(nome: str):
         return None
 
 # ===== COMANDO INFOMANGA =====
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import ContextTypes
-
 async def infomanga(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if not context.args:
         await update.message.reply_html(
@@ -557,6 +554,7 @@ app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("manga", manga))
 print("🤖 Bot rodando...")
 app.run_polling()
+
 
 
 
