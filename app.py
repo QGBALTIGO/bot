fb90566ea0c76a2d8dcdb3dcb663cfd0316f8386
QGBALTIGO <119024696+QGBALTIGO@users.noreply.github.com>
2026-02-22 23:02:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 from flask import Flask, request
 import requests
 import os
-from database import init_db, save_user_token
 
 app = Flask(__name__)
-init_db()
 
-CLIENT_ID = "36358"
-CLIENT_SECRET = "9ttdEOMhVL6aYP5vuJixCYFeqlmhIGBXsf898eie"
-REDIRECT_URI = "https://bot-production-1980.up.railway.app/callback"
+CLIENT_ID = os.environ.get("36358")
+CLIENT_SECRET = os.environ.get("9ttdEOMhVL6aYP5vuJixCYFeqlmhIGBXsf898eie")
+REDIRECT_URI = os.environ.get("https://bot-production-1980.up.railway.app/callback")
 
 @app.route("/")
 def home():
@@ -17,7 +15,9 @@ def home():
 @app.route("/callback")
 def callback():
     code = request.args.get("code")
-    telegram_id = request.args.get("state")
+
+    if not code:
+        return "Code não recebido", 400
 
     token_url = "https://anilist.co/api/v2/oauth/token"
 
@@ -30,11 +30,9 @@ def callback():
     }
 
     response = requests.post(token_url, json=data)
-    token = response.json().get("access_token")
+    return response.json()
 
-    if not token:
-        return "Erro ao obter token do AniList."
-
-    save_user_token(telegram_id, token)
-
-    return "Login realizado com sucesso! Pode voltar para o Telegram."
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    print("Iniciando servidor Flask...")
+    app.run(host="0.0.0.0", port=port)
