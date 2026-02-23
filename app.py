1c8bf5f6ec4943e5d65c53151034a3d896dad485
QGBALTIGO <119024696+QGBALTIGO@users.noreply.github.com>
2026-02-22 22:25:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request
 import requests
 import os
+from database import init_db, save_user_token
 
 app = Flask(__name__)
+init_db()
 
 CLIENT_ID = "36358"
 CLIENT_SECRET = "9ttdEOMhVL6aYP5vuJixCYFeqlmhIGBXsf898eie"
@@ -15,6 +17,7 @@ def home():
 @app.route("/callback")
 def callback():
     code = request.args.get("code")
+    telegram_id = request.args.get("state")
 
     token_url = "https://anilist.co/api/v2/oauth/token"
 
@@ -27,7 +30,11 @@ def callback():
     }
 
     response = requests.post(token_url, json=data)
-    return response.json()
+    token = response.json().get("access_token")
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    if not token:
+        return "Erro ao obter token do AniList."
+
+    save_user_token(telegram_id, token)
+
+    return "Login realizado com sucesso! Pode voltar para o Telegram."
