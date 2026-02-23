5db38236e4e24742f1e8798a7ef230dffa796ce7
QGBALTIGO <119024696+QGBALTIGO@users.noreply.github.com>
2026-02-22 20:14:26 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -470,13 +470,13 @@ async def anime(update: Update, context: ContextTypes.DEFAULT_TYPE):
     async with client:
         msg_id = await buscar_post(CANAL_ANIME, nome)
 
-    # ❌ NÃO ACHOU
+   # ❌ NÃO ACHOU
 if not msg_id:
-    await msg_busca.edit_text(
+    await msg_busca.delete()  # 👈 APAGA o "Buscando"
+    await update.message.reply_html(
         "🚫 <b>Nada por aqui…</b>\n\n"
         "O anime que você procurou não foi encontrado no canal.\n\n"
-        "✨ <i>Dica:</i> tente outro nome ou uma grafia diferente.",
-        parse_mode="HTML"
+        "✨ <i>Dica:</i> tente outro nome ou uma grafia diferente."
     )
     return
 
@@ -566,6 +566,7 @@ app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("manga", manga))
 print("🤖 Bot rodando...")
 app.run_polling()
+
 
 
 
