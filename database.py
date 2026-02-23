59e4dc33bd8b853fe830239e1f708f9860a25d94
QGBALTIGO <119024696+QGBALTIGO@users.noreply.github.com>
2026-02-23 19:27:09 -0400

    Update database.py

--- database.py
+++ database.py
@@ -1,51 +1,17 @@
 import sqlite3
 
-conn = sqlite3.connect("bot.db", check_same_thread=False)
-cursor = conn.cursor()
-
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS usuarios (
-    user_id INTEGER PRIMARY KEY,
-    nick TEXT,
-    nivel INTEGER,
-    xp INTEGER
-)
-""")
-
-conn.commit()
-
-import sqlite3
-
-# conecta (ou cria) o banco
-db = sqlite3.connect("database.db", check_same_thread=False)
+db = sqlite3.connect("bot.db", check_same_thread=False)
 cursor = db.cursor()
 
-# ===============================
-# TABELA DE USUÁRIOS
-# ===============================
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS users (
-    user_id INTEGER PRIMARY KEY,
+    telegram_id INTEGER PRIMARY KEY,
     nick TEXT,
-    coins INTEGER DEFAULT 0,
-    level INTEGER DEFAULT 1,
+    fav_name TEXT,
+    fav_image TEXT,
     commands INTEGER DEFAULT 0,
-    last_gacha INTEGER DEFAULT 0
-)
-""")
-
-# ===============================
-# TABELA DE PERSONAGENS CAPTURADOS
-# ===============================
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS user_characters (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id INTEGER,
-    character_id INTEGER,
-    character_name TEXT,
-    anime TEXT,
-    rarity TEXT,
-    quantity INTEGER DEFAULT 1
+    level INTEGER DEFAULT 1,
+    last_pedido INTEGER DEFAULT 0
 )
 """)
 
