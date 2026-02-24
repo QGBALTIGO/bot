0bf8ffe1b8232aaac4c920eb27141108067269e0
QGBALTIGO <119024696+QGBALTIGO@users.noreply.github.com>
2026-02-24 08:21:23 -0400

    Update database.py

--- database.py
+++ database.py
@@ -1,33 +1,15 @@
 import os
-import mysql.connector
 from urllib.parse import urlparse
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+print("DATABASE_URL =", DATABASE_URL)
+
 if not DATABASE_URL:
-    raise RuntimeError("DATABASE_URL não encontrada. Verifique as variáveis no Railway.")
+    raise RuntimeError("DATABASE_URL NÃO CHEGOU NO BOT")
 
 url = urlparse(DATABASE_URL)
 
-db = mysql.connector.connect(
-    host=url.hostname,
-    user=url.username,
-    password=url.password,
-    database=url.path.lstrip("/"),
-    port=url.port or 3306  # 👈 fallback de segurança
-)
-
-cursor = db.cursor(dictionary=True)
-
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS users (
-    user_id BIGINT PRIMARY KEY,
-    nick VARCHAR(255),
-    fav_character_name TEXT,
-    fav_character_image TEXT,
-    xp INT DEFAULT 0,
-    level INT DEFAULT 1
-)
-""")
-
-db.commit()
+print("HOST:", url.hostname)
+print("PORT:", url.port)
+print("DB:", url.path)
