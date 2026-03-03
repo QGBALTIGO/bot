5c61b0b2441921c0d8199412aae8bc52f0168327
QGBALTIGO <119024696+QGBALTIGO@users.noreply.github.com>
2026-03-03 13:37:19 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -5265,6 +5265,7 @@ def _start_webapp():
 
 if __name__ == "__main__":
     threading.Thread(target=_start_webapp, daemon=True).start()
+    app.run_polling(drop_pending_updates=True, allowed_updates=Update.ALL_TYPES)
 
 
 
@@ -5295,6 +5296,7 @@ async def safe_delete(msg):
             await safe_delete(msg)
     except Exception:
         pass
+
 
 
 
