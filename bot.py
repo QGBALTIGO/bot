8f10706dcd1bb6b3bf2cb836340915dbe596e91b
QGBALTIGO <119024696+QGBALTIGO@users.noreply.github.com>
2026-03-05 13:50:50 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,7 +28,7 @@ def main():
     tg_app = Application.builder().token(BOT_TOKEN).build()
 
     tg_app.add_handler(CommandHandler("start", start))
-    # tg_app.add_handler(CommandHandler("anime", anime))  # só quando existir
+    tg_app.add_handler(CommandHandler("anime", anime))  
 
     print("Bot + WebApp iniciado")
     tg_app.run_polling(drop_pending_updates=True)
