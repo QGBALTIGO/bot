a7a93435a99d0b3f52e1bac2999fa3a7f6e84edc
QGBALTIGO <119024696+QGBALTIGO@users.noreply.github.com>
2026-04-13 19:51:43 -0400

    Update inline_safebooru.py

--- commands/inline_safebooru.py
+++ commands/inline_safebooru.py
@@ -476,10 +476,9 @@ def _build_caption_from_anilist(query: str, meta: dict | None, post: dict) -> st
         lines = [
             f"🃏 <b>{name}</b>",
             "",
-            "<blockquote>",  f"<b>Gênero:</b> <code>{gender}</code>",
+            f"<blockquote><b>Gênero:</b> <code>{gender}</code>",
             f"<b>Nascimento:</b> <code>{birth}</code>",
-            f"<b>Favoritos:</b> <code>{favourites}</code>",
-            "</blockquote>",
+            f"<b>Favoritos:</b> <code>{favourites}</code></blockquote>",
             "",
             f"<b>Obra:</b> <code>{media_title}</code>",
             f"<b>Tipo:</b> <code>{media_type}</code>",
