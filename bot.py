7af08a83adbd5fd7977799a93da6c127c597d484
QGBALTIGO <119024696+QGBALTIGO@users.noreply.github.com>
2026-04-22 12:37:59 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -53,7 +53,7 @@ from commands.xcolecao import (
     xcolecao_f_callback,
     xcolecao_x_callback,
 )
-from commands.cccolecao import cccolecao
+from commands.cccolecao import colec
 
 from commands.loja import loja
 from commands.daily import daily
@@ -198,7 +198,7 @@ def register_commands(app: Application):
     # coleção
     app.add_handler(CommandHandler("colecao", colecao))
     app.add_handler(CommandHandler("xcolecao", xcolecao))
-    app.add_handler(CommandHandler("cccolecao", cccolecao))
+    app.add_handler(CommandHandler("colec", colec))
 
     # economia
     app.add_handler(CommandHandler("loja", loja))
